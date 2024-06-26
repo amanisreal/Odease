89ed2f71867b22f28c6688cd64ae1c607eff0f90
Aman <amanborkar995@gmail.com>
2024-06-26 22:31:14 +0530

    index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
-const userRouter = require('./src/models/user')
+const userRouter = require('./models/user')
 
 const port = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
 app.use(userRouter)
+
+app.get('/hi', (req, res) => {
+    res.send('Hello');
+})
 
 app.listen(port ,() => {
     console.log(`Server is running on port ${port}`)
