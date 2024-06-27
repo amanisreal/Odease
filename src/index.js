53d5236b8fec502a2254d0623714efed8f29370a
Aman <amanborkar995@gmail.com>
2024-06-27 12:43:36 +0530

    new" ;

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const userRouter = require('./models/user')
+const userRouter = require('./routes/users')
 
 const port = process.env.PORT || 3001;
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const User = require('../models/user');
+const auth = require('../middleware/auth')
 
 const router = new express.Router;
 
 //here login will create user   
 router.post('/login',  async (req, res) => {
+    console.log(req.body)
     try{
         const newUser = new User(req.body);
         newUser.save();
@@ -13,8 +16,25 @@ router.post('/login',  async (req, res) => {
     }
 })
 
-router.get('/user', async (req, res) => {
-    res.send('ijij')
+//get the user;
+router.get('/user', auth, async (req, res) => {
+    const user = req.user;
+    const token = req.token;
+    try{
+        res.status(200).send(user);
+    }catch(e){
+        res.status(400).send(e);
+    }
+})
+
+//log out the user
+router.post('/user', auth, async(req, res) => {
+    try{
+        const user = await User.findByIdAndDelete({_id: req.user._id});
+        res.status(200).send(user);
+    }catch(e){
+        res.status(400).send(e);
+    }
 })
 
 module.exports = router;
