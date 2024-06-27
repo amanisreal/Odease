df5fdc193436fc9257e7d48f25ab3a42a91b987f
Aman <amanborkar995@gmail.com>
2024-06-27 13:26:58 +0530

    cors

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const port = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
-app.use(userRouter)
+app.use(userRouter);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.options('*', cors())
+app.use(cors());
 
 app.get('/hi', (req, res) => {
     res.send('hello');
