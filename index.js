15cc579661401fe57bb752a8835070eaa007ae2b
Aman <amanborkar995@gmail.com>
2024-06-27 13:30:40 +0530

    checked

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const userRouter = require('./src/routes/users')
+const cors = require('cors')
 
 const port = process.env.PORT || 3001;
 
@@ -7,12 +8,19 @@ const app = express();
 app.use(express.json());
 app.use(userRouter);
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
-app.options('*', cors())
-app.use(cors());
+const corseOption = {
+    origin: '*',
+    Credentials: true,
+    OptionSuccessStatus: 200,
+}
+
+app.use(cors(corseOption))
 
 app.get('/hi', (req, res) => {
     res.send('hello');
