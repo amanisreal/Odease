485397c03d16ff0c829cebaa99273c3d56dbbcf1
Aman <amanborkar995@gmail.com>
2024-07-03 22:35:26 +0530

    cors

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const userRouter = require('./src/routes/users')
 const adminRouter = require('./src/routes/admins')
 const foodRouter = require('./src/routes/foodItems')
-const cors = require('cors')
 const connectBD = require('./src/database/mongoose')
 
 const port = process.env.PORT || 3001;
 
 const app = express();
-app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://odease-f.vercel.app"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.json());
 
 const connection = async () => {
