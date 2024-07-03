012bf1274b64d8c0302924efbc986ff179c24d86
Aman <amanborkar995@gmail.com>
2024-07-03 19:23:29 +0530

    bcrypt

--- src/models/admin.js
+++ src/models/admin.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt-nodejs')
 
 const adminSchema = new mongoose.Schema({
     userName: {
